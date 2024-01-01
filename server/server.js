923bead1694282284acd47e5b84a3bd44119c559
Talha4219 <139109096+Talha4219@users.noreply.github.com>
2024-01-01 12:15:05 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,24 +16,24 @@ import postCategoriesRoutes from "./routes/postCategoriesRoutes";
 
 dotenv.config();
 connectDB();
-const app = express();
-app.use(express.json()); 
-app.use(cors());    
+// const app = express();
+// app.use(express.json()); 
+// app.use(cors());    
 
 app.get("/", (req, res) => {     
   res.send("Server is running...");
 });
 
-app.use("/api/users", userRoutes);      
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/post-categories", postCategoriesRoutes);
+// app.use("/api/users", userRoutes);      
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/post-categories", postCategoriesRoutes);
 
-// static assets
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// // static assets
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(invalidPathHandler);
-app.use(errorResponserHandler);
+// app.use(invalidPathHandler);
+// app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;  
 
